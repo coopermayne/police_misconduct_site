9b3a2b599b28de3b1dbcb119ba460d7796dee520
Cooper Mayne <coopermayne@gmail.com>
2023-10-16 08:35:11 -0700

    good changes

--- eleventy.config.js
+++ eleventy.config.js
@@ -3,6 +3,16 @@ const navigation = require('@11ty/eleventy-navigation')
 const helpers = require('./utilities/filters/helpers')
 
 
+const markdownIt = require('markdown-it')
+const markdownItAttrs = require('markdown-it-attrs')
+
+const markdownItOptions = {
+  html: true,
+  breaks: true,
+  linkify: true
+}
+const markdownLib = markdownIt(markdownItOptions).use(markdownItAttrs)
+
 module.exports = function(eleventyConfig){
 
   eleventyConfig.addPlugin(navigation)
@@ -15,14 +25,18 @@ module.exports = function(eleventyConfig){
     eleventyConfig.addFilter('timestamp', dates.timestamp)
 
     // Remove whitespace from a string
-    // eleventyConfig.addNunjucksFilter('spaceless', helpers.spaceless)
+    eleventyConfig.addNunjucksFilter('spaceless', helpers.spaceless)
 
     eleventyConfig.addPassthroughCopy("src/assets/css/style.css");
     eleventyConfig.addPassthroughCopy("src/assets/images");
     eleventyConfig.addFilter("postDate", (dateObj) => {
       return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
     });
+
+    eleventyConfig.setLibrary('md', markdownLib)
+
     return {
+      markdownTemplateEngine: 'njk',
       dir: {
         input: "src",
         data: "_data",
